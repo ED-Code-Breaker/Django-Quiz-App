5184f397860b21df96d4c1c988bd9296d6f83847
ED-Code-Breaker <2011eltondias@gmail.com>
2020-10-15 17:25:19 +0530

    Completed Authentication

--- Django_Quiz/app/urls.py
+++ Django_Quiz/app/urls.py
@@ -4,4 +4,7 @@ from app import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('profile', views.profile, name='profile'),
+    path('handleSignup', views.handleSignup, name='handleSignup'),
+    path('handleLogin', views.handleLogin, name='handleLogin'),
 ]
--- Django_Quiz/app/views.py
+++ Django_Quiz/app/views.py
@@ -1,6 +1,58 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.
 
 def index(request):
+    return render(request, 'index.html')
+
+def profile(request):
+    return render(request, 'profile.html')
+
+def handleSignup(request):
+    if request.method == "POST":
+        uname = request.POST.get("uname")
+        email = request.POST.get("email")
+        pass1 = request.POST.get("pass1")
+        pass2 = request.POST.get("pass2")
+
+        if len(uname)>10:
+            messages.warning(request, "Username should be less than 10 characters")
+            return redirect('/')
+
+        if not uname.isalnum():
+            messages.warning(request, "Username should contain only letter and numbers")
+            return redirect('/')
+
+        if pass1 != pass2:
+            messages.warning(request, "Password do not match")
+            return redirect('/')
+
+        try:
+            if User.objects.get(uname= uname):
+                messages.warning(request, "Username already taken")
+                return redirect('/')
+        
+        except Exception as identifier:
+            pass
+
+        myuser = User.objects.create_user(uname, email, pass1)
+        myuser.save()
+        messages.success(request, "Signup Successful Please Login")
+        return redirect('/')
+    return render(request, 'index.html')
+
+def handleLogin(request):
+    if request.method == "POST":
+        loginusername = request.POST("uname")
+        loginpassword = request.POST('pass1')
+        user = authenticate(uname = loginusername, pass1 = loginpassword)
+        if user is not None:
+            login(request, user)
+            messages.info(request,"Login Success")
+            return redirect('/')
+        else:
+            messages.warning(request, "Invalid Credentials")
+            return redirect('/')
     return render(request, 'index.html')
